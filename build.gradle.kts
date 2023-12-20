c9b7d1f2348eaadc98ca705d450626b5ec0d49c9
cousim46 <wjdghldns24@naver.com>
2023-12-21 00:01:23 +0900

    feat : kotest를 위한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.6.0")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
 }
 
 tasks.withType<KotlinCompile> {
@@ -37,5 +42,8 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks.withType<Test> {
+    useJUnitPlatform()
+}
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
