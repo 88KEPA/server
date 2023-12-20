045db9ad07e7059c2cdfb7d128722aa1daed8ecb
cousim46 <wjdghldns24@naver.com>
2023-12-21 00:04:10 +0900

    feat : 회원 속성에 대한 enum 집합 파일 생성

---
+++ src/main/kotlin/com/kepa/domain/account/AccountFeature.kt
@@ -0,0 +1,13 @@
+package com.kepa.domain.account
+
+enum class Gender(
+    val value: String
+) {
+    MALE("남성"), FEMALE("여성")
+}
+
+enum class LoginType(
+    val type: String
+) {
+    KAKAO("카카오"), ORIGIN("사이트 회원가입")
+}
