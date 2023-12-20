7b00c89b1c971fc2c7ad4aa781407d301a9a6824
cousim46 <wjdghldns24@naver.com>
2023-12-20 23:59:26 +0900

    feat : 회원 엔티티 생성

---
+++ src/main/kotlin/com/kepa/domain/account/Account.kt
@@ -0,0 +1,35 @@
+package com.kepa.domain.account
+
+import com.kepa.common.BaseEntity
+import javax.persistence.Column
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+
+class Account(
+    @Column(nullable = false)
+    val name: String,
+    @Column(nullable = false)
+    val loginId: String,
+    @Column(nullable = false)
+    val password: String,
+    @Column(nullable = false)
+    val phone: String,
+    @Column(nullable = false)
+    val email: String,
+    @Column(nullable = false)
+    val zipCode: String,
+    @Column(nullable = false)
+    val jibunAddress: String,
+    @Column(nullable = false)
+    val jibunAddressDetail: String,
+    @Column(nullable = false)
+    val roadAddress: String,
+    @Column(nullable = false)
+    val roadAddressDetail: String,
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    val gender: Gender,
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    val loginType: LoginType,
+) : BaseEntity()
