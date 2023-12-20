21f69f5c99a3fedb7b8ed3966b72d4a41f990bbc
cousim46 <wjdghldns24@naver.com>
2023-12-21 01:29:24 +0900

    feat : 아이디 또는 이메일 존재여부 확인하는 메서드 쿼리 추가

--- src/main/kotlin/com/kepa/domain/account/AccountRepository.kt
+++ src/main/kotlin/com/kepa/domain/account/AccountRepository.kt
@@ -2,4 +2,6 @@ package com.kepa.domain.account
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface AccountRepository : JpaRepository<Account,Long>
+interface AccountRepository : JpaRepository<Account,Long> {
+    fun existsByLoginIdOrEmail(loginId: String, email: String) : Boolean
+}
