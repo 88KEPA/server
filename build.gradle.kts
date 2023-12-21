966e675676b29daad2cba2f7c5e8720c22525623
cousim46 <wjdghldns24@naver.com>
2023-12-22 00:00:26 +0900

    feat : 스웨거 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,10 +27,16 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //kotest
     testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
     testImplementation("io.kotest:kotest-assertions-core:5.7.2")
     testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
     testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
+
+    //스웨거
+    implementation(mapOf("group" to "io.springfox", "name" to "springfox-swagger-ui", "version" to "3.0.0"))
+    implementation(mapOf("group" to "io.springfox", "name" to "springfox-boot-starter", "version" to "3.0.0"))
 }
 
 tasks.withType<KotlinCompile> {
