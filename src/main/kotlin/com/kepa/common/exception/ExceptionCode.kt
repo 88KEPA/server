f2cd0ad53376d3f706da5831877801c03d231dc7
cousim46 <wjdghldns24@naver.com>
2023-12-27 21:35:04 +0900

    feat : 에러코드 추가

--- src/main/kotlin/com/kepa/common/exception/ExceptionCode.kt
+++ src/main/kotlin/com/kepa/common/exception/ExceptionCode.kt
@@ -5,7 +5,12 @@ import org.springframework.http.HttpStatus
 enum class ExceptionCode(
     val status: HttpStatus,
     val errorMessage: String,
+    val identity: Int
 ) {
-    ALREADY_INFORMATION(HttpStatus.CONFLICT, "이미 가입된 정보입니다."),
-    NOT_MATCH_PASSWORD_CONFIRM_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호와 비밀번호 재확인이 일치하지 않습니다.")
+    ALREADY_INFORMATION(HttpStatus.CONFLICT, "이미 가입된 정보입니다.",40901),
+    NO_AUTHENTICATION(HttpStatus.UNAUTHORIZED, "권한이 올바르지 않습니다.",40101),
+    NOT_MATCH_PASSWORD_CONFIRM_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호와 비밀번호 재확인이 일치하지 않습니다.",40001),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다.",40002),
+    NOT_EXSISTS_INFO(HttpStatus.BAD_REQUEST, "존재하지 않는 정보입니다.",40003),
+
 }
