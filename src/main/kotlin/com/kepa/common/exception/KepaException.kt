c308a68c93ee090530f9c6e6d4760d9a90815a2d
cousim46 <wjdghldns24@naver.com>
2023-12-27 21:41:30 +0900

    fix : 역할 분리로 인한 코드 삭제

--- src/main/kotlin/com/kepa/common/exception/KepaException.kt
+++ src/main/kotlin/com/kepa/common/exception/KepaException.kt
@@ -1,18 +1,6 @@
 package com.kepa.common.exception
 
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
-import java.lang.RuntimeException
-import java.time.LocalDateTime
 
 class KepaException(
     val exceptionCode: ExceptionCode,
-) : RuntimeException() {
-    val time: LocalDateTime = LocalDateTime.now()
-    val status: HttpStatus = exceptionCode.status
-    val errorMessage: String = exceptionCode.errorMessage
-    fun toResponse(): ResponseEntity<KepaException> {
-        return ResponseEntity.status(exceptionCode.status)
-            .body(this)
-    }
-}
+) : RuntimeException()
