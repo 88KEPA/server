1aa7592d504b239f05afd6d1e071d2353b556408
cousim46 <wjdghldns24@naver.com>
2023-12-27 21:37:19 +0900

    fix : 패키지 이동ˆ

--- src/main/kotlin/com/kepa/domain/trainer/TrainerRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.kepa.domain.trainer
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TrainerRepository : JpaRepository<Trainer,Long> {
-    fun existsByLoginIdOrEmail(loginId: String, email: String) : Boolean
-}
