104c89a51ec3ab5e9252d25a104de8fc8154b9db
cousim46 <wjdghldns24@naver.com>
2023-12-27 21:36:26 +0900

    feat : 이메일 중복 체크에 대한 비즈니스 로직 추가

---
+++ src/main/kotlin/com/kepa/application/trainer/TrainerReadService.kt
@@ -0,0 +1,20 @@
+package com.kepa.application.trainer
+
+import com.kepa.common.exception.ExceptionCode
+import com.kepa.common.exception.KepaException
+import com.kepa.domain.user.trainer.TrainerRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional(readOnly = true)
+@Service
+class TrainerReadService(
+    private val trainerRepository: TrainerRepository,
+    ) {
+
+    fun checkEmail(email: String) {
+        if(trainerRepository.existsByEmail(email)) {
+            throw KepaException(ExceptionCode.ALREADY_INFORMATION)
+        }
+    }
+}
