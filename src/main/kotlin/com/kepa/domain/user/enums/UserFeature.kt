230f6adc11ea8408f8dbc0dccd1822d59d014058
cousim46 <wjdghldns24@naver.com>
2023-12-27 21:38:18 +0900

    fix : 회원 특징에 대한 enum파일

---
+++ src/main/kotlin/com/kepa/domain/user/enums/UserFeature.kt
@@ -0,0 +1,19 @@
+
+enum class Gender(
+    val value: String
+) {
+    MALE("남성"), FEMALE("여성")
+}
+
+enum class LoginType(
+    val type: String
+) {
+    KAKAO("카카오"), ORIGIN("사이트 회원가입")
+}
+
+enum class Role(
+    val type: String
+) {
+    ADMIN("관리자"), USER("일반 사용자"), TRAINER("트레이너")
+}
+
