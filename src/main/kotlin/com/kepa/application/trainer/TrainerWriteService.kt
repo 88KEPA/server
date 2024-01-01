d6e705d3f2fa420f5cec558fcb3e545e75dbf40c
cousim46 <wjdghldns24@naver.com>
2024-01-02 00:38:54 +0900

    feat : 인증번호 생성 및 발송에 대한 비즈니스 로직

--- src/main/kotlin/com/kepa/application/trainer/TrainerWriteService.kt
+++ src/main/kotlin/com/kepa/application/trainer/TrainerWriteService.kt
@@ -1,12 +1,17 @@
 package com.kepa.application.trainer
 
 import com.kepa.application.trainer.dto.request.LoginInfo
+import com.kepa.application.trainer.dto.request.MessageContent
+import com.kepa.application.trainer.dto.request.RandomNumber
 import com.kepa.application.trainer.dto.request.TrainerJoin
 import com.kepa.application.trainer.dto.response.LoginToken
 import com.kepa.common.exception.ExceptionCode
 import com.kepa.common.exception.KepaException
+import com.kepa.domain.user.CertNumber
+import com.kepa.domain.user.CertNumberRepository
 import com.kepa.domain.user.trainer.TrainerRepository
 import com.kepa.token.TokenProvider
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -16,25 +21,42 @@ import java.util.*
 @Transactional
 class TrainerWriteService(
     private val trainerRepository: TrainerRepository,
+    private val certNumberRepository: CertNumberRepository,
     private val bCryptPasswordEncoder: BCryptPasswordEncoder,
     private val tokenProvider: TokenProvider,
+    private val applicationEventPublisher: ApplicationEventPublisher,
 ) {
     fun join(trainerJoin: TrainerJoin) {
-        trainerJoin.checkPassword()
-        trainerJoin.checkDuplicateInformation(
-            trainerRepository.existsByEmail(
+        require(
+            !trainerRepository.existsByEmail(
                 trainerJoin.email
             )
-        )
+        ) {
+            throw KepaException(ExceptionCode.ALREADY_INFORMATION)
+        }
+
         trainerRepository.save(trainerJoin.create(bCryptPasswordEncoder.encode(trainerJoin.password)))
     }
 
     fun login(loginInfo: LoginInfo, now: Date): LoginToken {
         val trainer = trainerRepository.findByEmail(loginInfo.email)
             ?: throw KepaException(ExceptionCode.NOT_EXSISTS_INFO)
-        if(!bCryptPasswordEncoder.matches(loginInfo.password, trainer.password)) {
+        if (!bCryptPasswordEncoder.matches(loginInfo.password, trainer.password)) {
             throw KepaException(ExceptionCode.NOT_EXSISTS_INFO)
         }
         return tokenProvider.getToken(trainer.email, trainer.role.name, now)
+    }
+
+    fun checkNumber(receiverPhoneNumber: String, userId: String) {
+        val certNumber = RandomNumber.create()
+        if(certNumberRepository.existsByReceiverEmail(userId)) {
+            certNumberRepository.deleteByReceiverEmail(userId)
+        }
+        certNumberRepository.save(CertNumber(certNumber,receiverPhoneNumber))
+        applicationEventPublisher.publishEvent(MessageContent(
+            certNumber = RandomNumber.create(),
+            receiverPhoneNumber = receiverPhoneNumber,
+            userId = userId,
+        ))
     }
 }
