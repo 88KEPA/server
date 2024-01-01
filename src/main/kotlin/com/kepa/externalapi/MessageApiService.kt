ca9639c6ac3e83dca71e8d8154537cf549799960
cousim46 <wjdghldns24@naver.com>
2024-01-02 00:37:18 +0900

    feat : 메시지 전송에 대한 비즈니스 로직

---
+++ src/main/kotlin/com/kepa/externalapi/MessageApiService.kt
@@ -0,0 +1,42 @@
+package com.kepa.externalapi
+
+import com.kepa.application.trainer.dto.request.MessageContent
+import com.kepa.application.trainer.dto.response.MessageSendStatus
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.event.TransactionalEventListener
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestTemplate
+
+@Transactional
+@Service
+class MessageApiService(
+    @Value("\${aligo.key}")
+    private val aligoKey: String,
+    @Value("\${aligo.send-url}")
+    private val aligoSendUrl: String,
+    @Value("\${aligo.send-phone-number}")
+    private val sendPhoneNumber: String,
+) {
+
+    @TransactionalEventListener
+    fun joinSendMessage(messageContent: MessageContent) {
+        val restTemplate = RestTemplate()
+        val requestValue : MultiValueMap<String, String> = LinkedMultiValueMap()
+        requestValue.add("key", aligoKey)
+        requestValue.add("user_id", messageContent.userId)
+        requestValue.add("sender", sendPhoneNumber)
+        requestValue.add("receiver", "01094905048")
+        requestValue.add("msg", "[KEPA]본인확인 인증번호 [123456]를 화면에 입력해주세요.")
+        val httpEntity: HttpEntity<MultiValueMap<String, String>> = HttpEntity(requestValue)
+        println("testtest")
+        val exchange = restTemplate.exchange(
+            aligoSendUrl, HttpMethod.POST, httpEntity, MessageSendStatus::class.java
+        )
+        println(exchange)
+    }
+}
