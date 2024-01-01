f794a5f0d67ebd630f206a68fbd558b7cf7e7052
cousim46 <wjdghldns24@naver.com>
2024-01-02 00:38:37 +0900

    feat : 외부 API 응답값 매핑 dto 추가

--- src/main/kotlin/com/kepa/application/trainer/dto/response/TrainerResponse.kt
+++ src/main/kotlin/com/kepa/application/trainer/dto/response/TrainerResponse.kt
@@ -6,3 +6,11 @@ data class LoginToken(
     val refreshToken: String,
 )
 
+data class MessageSendStatus(
+    val result_code: Int,
+    val message: String,
+    val msg_id: Int,
+    val success_cnt: Int,
+    val error_cnt: Int,
+    val msg_type: String,
+)
