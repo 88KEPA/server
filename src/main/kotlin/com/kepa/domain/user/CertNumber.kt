846561365c89f673c3a8f67af17bd3e23f91c8c1
jhw1015 <jhw1015@bank-mall.co.kr>
2024-01-14 14:47:16 +0900

    feat : 인증번호에 대한 컬럼 추가

--- src/main/kotlin/com/kepa/domain/user/CertNumber.kt
+++ src/main/kotlin/com/kepa/domain/user/CertNumber.kt
@@ -1,11 +1,14 @@
 package com.kepa.domain.user
 
 import com.kepa.common.BaseEntity
-import com.kepa.domain.user.trainer.Trainer
 import javax.persistence.*
 
 @Entity
 class CertNumber(
-    val number: Int,
+    @Column(nullable = false)
+    val receiverPhoneNumber: String,
+    @Column(nullable = false)
     val receiverEmail: String,
+    @Column(nullable = false)
+    val number: Int
 ): BaseEntity()
