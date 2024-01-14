24e224ad94b4cc456b501279a3eea75c140c9432
jhw1015 <jhw1015@bank-mall.co.kr>
2024-01-14 14:48:36 +0900

    feat : 이메일과 전화번호로 인증번호 찾는 메서드 쿼리 추가

--- src/main/kotlin/com/kepa/domain/user/CertNumberRepository.kt
+++ src/main/kotlin/com/kepa/domain/user/CertNumberRepository.kt
@@ -6,4 +6,6 @@ interface CertNumberRepository: JpaRepository<CertNumber, Long> {
 
     fun existsByReceiverEmail(email: String): Boolean
     fun deleteByReceiverEmail(email: String)
+
+    fun findByReceiverEmailAndReceiverPhoneNumber(email: String, phoneNumber: String) : CertNumber?
 }
