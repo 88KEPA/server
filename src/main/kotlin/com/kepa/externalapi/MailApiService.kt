a4d707464abe0f2a7ea0be67abd1783115d0b46c
jhw1015 <jhw1015@bank-mall.co.kr>
2024-01-14 23:38:53 +0900

    feat : 메일 발송에 대한 비즈니스 로직

---
+++ src/main/kotlin/com/kepa/externalapi/MailApiService.kt
@@ -0,0 +1,28 @@
+package com.kepa.externalapi
+
+import com.kepa.application.trainer.dto.request.MailContent
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
+import org.springframework.transaction.event.TransactionalEventListener
+
+@Service
+class MailApiService(
+    private val javaMailSender: JavaMailSender,
+    @Value("\${spring.mail.username}")
+    private val senderEmail: String,
+) {
+
+    @TransactionalEventListener
+    fun sendMail(mailContent: MailContent) {
+        val simpleMailMessage = SimpleMailMessage()
+        simpleMailMessage.setTo(mailContent.email)
+        simpleMailMessage.from = senderEmail
+        simpleMailMessage.subject = "[KEPA] 이메일 인증번호"
+        simpleMailMessage.text = "인증번호 [${mailContent.certNumber}]"
+        println(senderEmail)
+        println(mailContent.email)
+        javaMailSender.send(simpleMailMessage)
+    }
+}
