aa60f0c0452636515ec4161a8bc810f605f0ac10
jhw1015 <jhw1015@bank-mall.co.kr>
2024-01-14 12:48:56 +0900

    feat : 랜덤 한 값 추가

--- src/main/kotlin/com/kepa/externalapi/MessageApiService.kt
+++ src/main/kotlin/com/kepa/externalapi/MessageApiService.kt
@@ -4,7 +4,9 @@ import com.kepa.application.trainer.dto.request.MessageContent
 import com.kepa.application.trainer.dto.response.MessageSendStatus
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.event.TransactionalEventListener
@@ -27,16 +29,16 @@ class MessageApiService(
     fun joinSendMessage(messageContent: MessageContent) {
         val restTemplate = RestTemplate()
         val requestValue : MultiValueMap<String, String> = LinkedMultiValueMap()
+        val header = HttpHeaders()
+        header.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE)
         requestValue.add("key", aligoKey)
         requestValue.add("user_id", messageContent.userId)
         requestValue.add("sender", sendPhoneNumber)
-        requestValue.add("receiver", "01094905048")
-        requestValue.add("msg", "[KEPA]본인확인 인증번호 [123456]를 화면에 입력해주세요.")
-        val httpEntity: HttpEntity<MultiValueMap<String, String>> = HttpEntity(requestValue)
-        println("testtest")
-        val exchange = restTemplate.exchange(
+        requestValue.add("receiver", messageContent.receiverPhoneNumber)
+        requestValue.add("msg", "[KEPA]본인확인 인증번호 [${messageContent.randomNumber}]를 화면에 입력해주세요.")
+        val httpEntity: HttpEntity<MultiValueMap<String, String>> = HttpEntity(requestValue,header)
+        restTemplate.exchange(
             aligoSendUrl, HttpMethod.POST, httpEntity, MessageSendStatus::class.java
         )
-        println(exchange)
     }
 }
