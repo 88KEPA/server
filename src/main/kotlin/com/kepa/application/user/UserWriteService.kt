ad2e59c7223272c8d27b15ef96d2eb7aa4f634a0
jhw1015 <jhw1015@bank-mall.co.kr>
2024-01-17 22:47:28 +0900

    feat : 회원 로그인에 대한 APi 및 비즈니스 로직 추가

---
+++ src/main/kotlin/com/kepa/application/user/UserWriteService.kt
@@ -0,0 +1,28 @@
+package com.kepa.application.user
+
+import com.kepa.application.user.trainer.dto.request.LoginInfo
+import com.kepa.application.user.trainer.dto.response.LoginToken
+import com.kepa.common.exception.ExceptionCode
+import com.kepa.common.exception.KepaException
+import com.kepa.domain.user.trainer.TrainerRepository
+import com.kepa.token.TokenProvider
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class UserWriteService(
+    private val trainerRepository: TrainerRepository,
+    private val bCryptPasswordEncoder: BCryptPasswordEncoder,
+    private val tokenProvider: TokenProvider,
+) {
+
+    fun login(loginInfo: LoginInfo, now: Date): LoginToken {
+        val trainer = trainerRepository.findByEmail(loginInfo.email)
+            ?: throw KepaException(ExceptionCode.NOT_MATCH_ID_OR_PASSWORD)
+        if (!bCryptPasswordEncoder.matches(loginInfo.password, trainer.password)) {
+            throw KepaException(ExceptionCode.NOT_MATCH_ID_OR_PASSWORD)
+        }
+        return tokenProvider.getToken(trainer.email, trainer.role.name, now)
+    }
+}
