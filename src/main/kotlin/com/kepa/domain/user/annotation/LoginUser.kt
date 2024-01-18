35016129313a00a75332e7b8b95da5fb039cb32b
cousim46 <wjdghldns24@naver.com>
2024-01-18 19:11:25 +0900

    feat : 로그인 정보를 받기위한 어노테이션

---
+++ src/main/kotlin/com/kepa/domain/user/annotation/LoginUser.kt
@@ -0,0 +1,6 @@
+package com.kepa.domain.user.annotation
+
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.ANNOTATION_CLASS)
+annotation class LoginUser
