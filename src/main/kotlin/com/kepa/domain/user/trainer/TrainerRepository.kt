8d26f26aabac1af28af5c6a99bd5554dcfc4467c
cousim46 <wjdghldns24@naver.com>
2024-01-18 19:14:20 +0900

    fix : 메서드쿼리 변경

--- src/main/kotlin/com/kepa/domain/user/trainer/TrainerRepository.kt
+++ src/main/kotlin/com/kepa/domain/user/trainer/TrainerRepository.kt
@@ -1,10 +1,11 @@
 package com.kepa.domain.user.trainer
 
+import Role
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.query.Param
 
 interface TrainerRepository : JpaRepository<Trainer,Long> {
     fun existsByEmail(email: String) : Boolean
 
-    fun findByEmail(@Param("email")email: String?) : Trainer?
+    fun findByEmailAndRole(@Param("email")email: String?, @Param("role")role: Role) : Trainer?
 }
