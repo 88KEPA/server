7693e58a141f2ff510158d3f944ac6f423ec02e9
cousim46 <wjdghldns24@naver.com>
2024-01-21 18:55:30 +0900

    feat : 이메일 찾기에 대한 API

--- src/main/kotlin/com/kepa/application/user/trainer/TrainerReadService.kt
+++ src/main/kotlin/com/kepa/application/user/trainer/TrainerReadService.kt
@@ -10,11 +10,26 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class TrainerReadService(
     private val trainerRepository: TrainerRepository,
-    ) {
+) {
+    companion object {
+        val FIRST_INDEX = 0
+        val END_INDEX = 4
+    }
 
     fun checkEmail(email: String) {
-        if(trainerRepository.existsByEmail(email)) {
+        if (trainerRepository.existsByEmail(email)) {
             throw KepaException(ExceptionCode.ALREADY_INFORMATION)
         }
+    }
+
+    fun findEmail(phoneNumber: String): String {
+        val trainer = trainerRepository.findByPhone(phoneNumber) ?: throw KepaException(ExceptionCode.NOT_EXSISTS_INFO)
+        return trainer.email.masking(FIRST_INDEX, END_INDEX)
+    }
+
+    fun String.masking(startIndex: Int, endIndex: Int): String {
+        val substringTarget = this.substring(startIndex, endIndex)
+        val substringOrigin = this.substring(endIndex)
+        return "*".repeat(substringTarget.length) + substringOrigin
     }
 }
