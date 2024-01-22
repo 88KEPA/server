5b816b6587a06fbb72961324d9127c1ea4ae720a
cousim46 <wjdghldns24@naver.com>
2024-01-22 22:46:30 +0900

    feat : 설정 변경

--- src/main/kotlin/com/kepa/config/WebMvcConfig.kt
+++ src/main/kotlin/com/kepa/config/WebMvcConfig.kt
@@ -2,20 +2,23 @@ package com.kepa.config
 
 import com.kepa.domain.user.trainer.TrainerRepository
 import com.kepa.security.LoginInterception
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfig(
-    private val trainerRepository: TrainerRepository
+    private val trainerRepository: TrainerRepository,
+    @Value("\${cors.url}")
+    private val corsAccessUrl: String,
 ) : WebMvcConfigurer {
+
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:9402")
+            .allowedOrigins(corsAccessUrl)
             .allowedMethods("OPTIONS","GET","POST","PUT","DELETE");
     }
 
