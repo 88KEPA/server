fadec7cd48a68bd32794648a3ad9516eee294dcd
cousim46 <wjdghldns24@naver.com>
2024-01-22 22:14:29 +0900

    feat : 유저 정보 상세보기 ResponseDto 생성

---
+++ src/main/kotlin/com/kepa/application/user/dto/response/DetailInfo.kt
@@ -0,0 +1,35 @@
+package com.kepa.application.user.dto.response
+
+import Gender
+import Role
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.kepa.domain.user.trainer.Trainer
+import java.time.LocalDate
+
+data class DetailInfo(
+    val name: String,
+    val gender: Gender,
+    @JsonFormat(pattern = "yyyy.MM.dd")
+    val birth: LocalDate,
+    val address: String,
+    val addressMeta: String,
+    val addressDetail: String,
+    val phone: String,
+    val role: Role
+) {
+    companion object {
+        fun create(trainer: Trainer) : DetailInfo{
+            return DetailInfo(
+                name = trainer.name,
+                gender =  trainer.gender,
+                birth = trainer.birth,
+                address = trainer.address,
+                addressMeta = trainer.addressMeta,
+                addressDetail = trainer.addressDetail,
+                phone = trainer.phone,
+                role =  trainer.role
+            )
+        }
+    }
+}
+
