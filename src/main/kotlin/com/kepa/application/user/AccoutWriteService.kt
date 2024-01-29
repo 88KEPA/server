7e0da787e09307cc4c0e80022adadcf2041d14f1
cousim46 <wjdghldns24@naver.com>
2024-01-29 22:50:44 +0900

    fix : trainer -> account로 변경

--- src/main/kotlin/com/kepa/application/user/AccoutWriteService.kt
+++ src/main/kotlin/com/kepa/application/user/AccoutWriteService.kt
@@ -9,6 +9,7 @@ import com.kepa.common.exception.ExceptionCode.NOT_MATCH_ID_OR_PASSWORD
 import com.kepa.common.exception.KepaException
 import com.kepa.domain.user.account.Account
 import com.kepa.domain.user.account.AccountRepository
+import com.kepa.domain.user.account.RefreshTokenRepository
 import com.kepa.token.TokenProvider
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -31,24 +32,22 @@ class AccoutWriteService(
         require(bCryptPasswordEncoder.matches(loginInfo.password, trainer.password)) {
             throw KepaException(NOT_MATCH_ID_OR_PASSWORD)
         }
-        if(refreshTokenRepository.existsByEmailAndRole(trainer.email,trainer.role)) {
+
+        if (refreshTokenRepository.existsByEmailAndRole(trainer.email, trainer.role)) {
             refreshTokenRepository.deleteByEmail(trainer.email)
         }
         return tokenProvider.getToken(trainer.email, trainer.role.name, now)
     }
 
     fun logout(loginUserInfo: LoginUserInfo) {
-        val trainer = accountRepository.findByIdOrNull(loginUserInfo.id)
+        val account = accountRepository.findByIdOrNull(loginUserInfo.id)
             ?: throw KepaException(NOT_MATCH_ID_OR_PASSWORD)
-        refreshTokenRepository.deleteByEmail(trainer.email)
+        refreshTokenRepository.deleteByEmail(account.email)
     }
 
-
     fun getToken(id: Long, role: Role, now: Date): LoginToken {
-        if(role == Role.TRAINER) {
-            val account : Account = accountRepository.findByIdOrNull(id) ?: throw KepaException(NOT_EXSISTS_INFO)
-            return tokenProvider.getToken(account.email, account.role.name, now)
-        }
-        throw KepaException(NOT_EXSISTS_INFO)
+        val account: Account = accountRepository.findByIdOrNull(id) ?: throw KepaException(NOT_EXSISTS_INFO)
+        return tokenProvider.getToken(account.email, account.role.name, now)
+
     }
 }
