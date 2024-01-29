fa51a7beb1a98801ef337f8249e0256fd5f4b681
cousim46 <wjdghldns24@naver.com>
2024-01-29 22:55:05 +0900

    feat : 제휴업체 엔티티

---
+++ src/main/kotlin/com/kepa/domain/partner/Partner.kt
@@ -0,0 +1,25 @@
+package com.kepa.domain.partner
+
+import com.kepa.common.BaseEntity
+import com.kepa.domain.partner.enums.ApproveStatus
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+
+@Entity
+class Partner(
+    @Column(nullable = false)
+    val name: String,
+    @Column(nullable = false)
+    val phone: String,
+    @Column(nullable = false)
+    val email: String,
+    @Column(nullable = false)
+    val content: String,
+    @Column(nullable = false)
+    val organization: String,
+    @Enumerated(EnumType.STRING)
+    val approveStatus: ApproveStatus = ApproveStatus.READY,
+) : BaseEntity(){
+}
