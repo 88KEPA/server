b0ccdad3a5f24639f9bf35cf9322f6bdfc17cf99
cousim46 <wjdghldns24@naver.com>
2024-01-31 00:34:13 +0900

    feat : 트레이너 목록 조회 API

--- src/main/kotlin/com/kepa/application/user/admin/AdminController.kt
+++ src/main/kotlin/com/kepa/application/user/admin/AdminController.kt
@@ -1,8 +1,10 @@
 package com.kepa.application.user.admin
 
 import com.kepa.application.user.AccountReadService
+import com.kepa.application.user.admin.dto.request.PageRequest
 import com.kepa.application.user.dto.request.LoginUserInfo
 import com.kepa.application.user.dto.response.DetailInfo
+import com.kepa.application.user.dto.response.PageResponse
 import com.kepa.domain.user.annotation.LoginUser
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
@@ -14,11 +16,18 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/admin")
 class AdminController(
-    private val accountReadService: AccountReadService
+    private val accountReadService: AccountReadService,
+    private val adminReadService: AdminReadService,
 ) {
     @ApiOperation(value ="관리자 정보보기")
     @GetMapping("/info")
     fun getDetailInfo(@LoginUser loginUserInfo: LoginUserInfo) : DetailInfo {
         return DetailInfo.create(accountReadService.getDetailInfo(loginUserInfo.id))
+    }
+
+    @ApiOperation(value = "트레이너 목록")
+    @GetMapping("/list/trainer")
+    fun getTrainer(@LoginUser loginUserInfo: LoginUserInfo, pageRequest: PageRequest) : PageResponse {
+        return adminReadService.getJoinTrainer(page = pageRequest.page, limit = pageRequest.limit)
     }
 }
