18a054f3c11b274575d9cad7d774671fd43bb952
cousim46 <wjdghldns24@naver.com>
2024-02-02 01:05:08 +0900

    fix : cors 수정

--- src/main/kotlin/com/kepa/config/WebMvcConfig.kt
+++ src/main/kotlin/com/kepa/config/WebMvcConfig.kt
@@ -15,10 +15,10 @@ class WebMvcConfig(
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:9402","http://localhost:9401" ,"https://www.kepa.associates/")
+            .allowedOrigins("http://localhost:9402","http://localhost:9401", "https://www.kepa.associates/")
             .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("*")
-            .exposedHeaders("*")
+            .allowedHeaders("Authorization", "Origin","X-Requested-With" ,"Content-Type", "Accept")
+            .exposedHeaders("Authorization", "Origin","X-Requested-With" ,"Content-Type", "Accept")
             .allowCredentials(true)
             .maxAge(3600)
 
