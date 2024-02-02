54643cd60d55692491a6319a201251a426862348
cousim46 <wjdghldns24@naver.com>
2024-02-03 04:36:02 +0900

    fix : 약관 내용에 대한 데이터타입 변경

--- src/main/kotlin/com/kepa/domain/user/terms/Terms.kt
+++ src/main/kotlin/com/kepa/domain/user/terms/Terms.kt
@@ -3,10 +3,12 @@ package com.kepa.domain.user.terms
 import com.kepa.common.BaseEntity
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.Lob
 
 @Entity
 class Terms(
-    @Column(name = "TEXT")
+    @Lob
+    @Column(columnDefinition = "TEXT")
     val content: String,
     val title: String,
 ) : BaseEntity(){
