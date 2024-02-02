771b1e51242804f93aeb865da7040f8e181c516b
cousim46 <wjdghldns24@naver.com>
2024-02-03 00:23:22 +0900

    fix : cors 오류 해결

--- src/main/kotlin/com/kepa/config/WebMvcConfig.kt
+++ src/main/kotlin/com/kepa/config/WebMvcConfig.kt
@@ -4,29 +4,14 @@ import com.kepa.domain.user.account.AccountRepository
 import com.kepa.security.LoginInterception
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
-import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-/*@Configuration
+@Configuration
 class WebMvcConfig(
     private val accountRepository: AccountRepository,
 ) : WebMvcConfigurer {
-
-
-    override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:9402","http://localhost:9401", "https://www.kepa.associates/")
-            .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
-            .allowedHeaders("Authorization", "Origin","X-Requested-With" ,"Content-Type", "Accept")
-            .exposedHeaders("Authorization", "Origin","X-Requested-With" ,"Content-Type", "Accept")
-            .allowCredentials(true)
-            .maxAge(3600)
-
-    }
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(LoginInterception(accountRepository))
     }
 }
-
- */
