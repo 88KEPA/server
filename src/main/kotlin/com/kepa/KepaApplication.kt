016e91de625d46a8a0d664512df8e6a82a26337c
cousim46 <wjdghldns24@naver.com>
2024-02-05 22:47:06 +0900

    feat : 한국시간으로 설정

--- src/main/kotlin/com/kepa/KepaApplication.kt
+++ src/main/kotlin/com/kepa/KepaApplication.kt
@@ -3,11 +3,17 @@ package com.kepa
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.*
+import javax.annotation.PostConstruct
 
 @EnableJpaAuditing
 @SpringBootApplication
 class KepaApplication
 
+@PostConstruct
+fun started() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+}
 fun main(args: Array<String>) {
     runApplication<KepaApplication>(*args)
 }
