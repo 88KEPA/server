06df44fe584d9f4fab50409b8b7f7f26275e82f7
cousim46 <wjdghldns24@naver.com>
2024-02-07 01:34:32 +0900

    feat : 회원 탈퇴시 soft delete를 위한 쿼리

--- src/main/kotlin/com/kepa/domain/user/account/AccountRepository.kt
+++ src/main/kotlin/com/kepa/domain/user/account/AccountRepository.kt
@@ -2,6 +2,7 @@ package com.kepa.domain.user.account
 
 import Role
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
@@ -20,4 +21,11 @@ interface AccountRepository : JpaRepository<Account,Long> {
 
     @Query(value = "SELECT a FROM Account a WHERE (a.email = :keyword OR a.name = :keyword OR a.phone = :keyword) AND a.role = :role")
     fun findAllByEmailOrNameOrPhone(@Param("keyword")keyword: String, @Param("role") role: Role): List<Account>
+
+    @Modifying
+    @Query(value = """
+        UPDATE Account SET name = null, phone = null, birth = null, password = null, address = null, addressDetail = null,
+        addressMeta = null,email = null, withdrawAt = now(), role = 'WITHDRAW' where id = :id
+        """)
+    fun withdrawAccount(@Param("id") id: Long)
 }
