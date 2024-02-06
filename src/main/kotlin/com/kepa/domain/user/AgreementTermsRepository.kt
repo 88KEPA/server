19cdfc10939437681a0c1553dc1af3dc8ab7e404
cousim46 <wjdghldns24@naver.com>
2024-02-07 01:34:56 +0900

    feat : 회원 탈퇴시 약관 동의한 내용 삭제하는 메서드쿼리 추가

--- src/main/kotlin/com/kepa/domain/user/AgreementTermsRepository.kt
+++ src/main/kotlin/com/kepa/domain/user/AgreementTermsRepository.kt
@@ -3,4 +3,6 @@ package com.kepa.domain.user
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AgreementTermsRepository : JpaRepository<AgreementTerms, Long> {
+
+    fun deleteByAccountId(accountId: Long)
 }
