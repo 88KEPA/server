c332ed191005e4ac27af7f547f22c801b97a63b9
cousim46 <wjdghldns24@naver.com>
2024-02-07 02:14:51 +0900

    fix : 쿼리수정

--- src/main/kotlin/com/kepa/domain/user/account/RefreshTokenRepository.kt
+++ src/main/kotlin/com/kepa/domain/user/account/RefreshTokenRepository.kt
@@ -1,12 +1,11 @@
 package com.kepa.domain.user.account
 
-import Role
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface RefreshTokenRepository : JpaRepository<RefreshToken, Long> {
 
-    fun existsByEmailAndRole(email: String, role: Role): Boolean
+    fun existsByAccountId(accountId: Long): Boolean
 
-    fun findByEmailAndRole(email:String, role: Role): RefreshToken?
-    fun deleteByEmail(email: String)
+    fun findByAccountId(accountId: Long): RefreshToken?
+    fun deleteByAccountId(accountId: Long)
 }
