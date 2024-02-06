a2d497bbce9470acb9ed32de14cf2925f56de9ff
cousim46 <wjdghldns24@naver.com>
2024-02-07 01:34:04 +0900

    feat : 탈퇴 회원에 대한 상태값 추가

--- src/main/kotlin/com/kepa/domain/user/enums/UserFeature.kt
+++ src/main/kotlin/com/kepa/domain/user/enums/UserFeature.kt
@@ -14,7 +14,7 @@ enum class LoginType(
 enum class Role(
     val type: String
 ) {
-    ADMIN("관리자"), USER("일반 사용자"), TRAINER("트레이너"), SUPER_ADMIN("슈퍼 관리자")
+    ADMIN("관리자"), USER("일반 사용자"), TRAINER("트레이너"), SUPER_ADMIN("슈퍼 관리자"), WITHDRAW("탈퇴 회원")
 }
 
 enum class CertType(
