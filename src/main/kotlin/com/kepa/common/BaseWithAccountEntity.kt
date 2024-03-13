d7f526e9ee517d1586ddefd578654d6f5f16db9e
cousim46 <wjdghldns24@naver.com>
2024-03-14 01:50:53 +0900

    feat : 수정자, 생성자가 필요한 엔티티를 위한 추상클래스 생성

---
+++ src/main/kotlin/com/kepa/common/BaseWithAccountEntity.kt
@@ -0,0 +1,28 @@
+package com.kepa.common
+
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@MappedSuperclass
+@EntityListeners(value = [AuditingEntityListener::class])
+abstract class BaseWithAccountEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now(),
+    @LastModifiedDate
+    var updatedAt: LocalDateTime = LocalDateTime.now(),
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    var createdById: Long = 0,
+    @LastModifiedBy
+    @Column(nullable = false)
+    var updatedById: Long = 0
+)
